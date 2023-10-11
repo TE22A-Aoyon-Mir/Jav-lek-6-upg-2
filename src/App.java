aa3599201bb94888f42391f486c099aef1e06e75
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-10-11 10:41:06 +0200

    Gjort klar del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner t = new Scanner(System.in);
+        
+        System.out.println("Del a");
+        System.out.println("Ange ett tal: ");
+        int tal1 = t.nextInt();
+        int x = 500;
+        tal1+=x;
+        tal1/=1000;
+        tal1*=1000;
+        System.out.println("Talet avrundat blir "+tal1);
     }
 }
